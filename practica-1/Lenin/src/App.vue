48b087896569d9405987a025c8ad0f3606cef715
Marco Ambuludi <lenin.a@andeantc.com>
2024-09-03 10:15:47 -0500

    Files homework

---
+++ practica-1/Lenin/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <head></head>
+    <body>
+
+      <!-- Primera barra (negra) -->
+      <div class="top-bar-black">
+          <div style="float: left;" class="logo">TRAIN HARD</div>
+          <div class="social-icons">
+            <a href="#">Facebook</a>
+            <a href="#">Twitter</a>
+            <a href="#">Instagram</a>
+            
+          </div>
+      </div>
+  
+      <!-- Segunda barra (blanca) -->
+      <div class="top-bar-white">
+          <a href="#">HOME</a>
+          <a href="#">SHOP</a>
+          <a href="#">ABOUT</a>
+          <a href="#">CONTACT</a>
+          <a href="#">FAQ</a>
+          <a href="#">
+          </a>
+          
+      </div>
+  
+      <!-- Imagen principal -->
+      <div class="main-image"></div>
+  
+      <!-- Footer con tres cajas -->
+      <div class="footer">
+          <div class="footer-box box-grey">PROGRESSION</div>
+          <div class="footer-box box-white">WORKOUT</div>
+          <div class="footer-box box-red">NUTRITION</div>
+      </div>
+  
+  </body>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/styles/styles.scss';
+</style>
+
+
+<script>
+
+
+export default {
+  
+  name: 'App',
+  components: {
+  }
+};
+</script>
---
+++ practica-1/Lenin/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'; 
+
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPhone,faInstagram)
+
+
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
